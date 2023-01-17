990c39c9ba7874e8e776a7b27b74a2fff9875460
nominlang <nominsumiya@gmail.com>
2023-01-18 00:16:31 +0100

    Solved lav

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,22 +1,30 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+   this.intervalId = setInterval(() => {
+this.currentTime++
+if(printTimeCallback){
+  printTimeCallback()
+    }
+   }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+   let minutes = this.currentTime / 60
+   return Math.floor(minutes)
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime % 60
+   return Math.floor(seconds)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    
   }
 
   stop() {
